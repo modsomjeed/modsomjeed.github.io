0624878429796a57ac006ea09dc1126e51d14b43
modsomjeed <nitcharee.kwang@odds.team>
2025-12-19 16:59:21 +0700

    trigger build

--- src/portfolio.js
+++ src/portfolio.js
@@ -23,7 +23,7 @@ const greeting = {
   username: "Nitcharee Patkunjiratawol",
   title: "Hi all, I'm Kwang",
   subTitle: emoji(
-    "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
+    "TEST TEST A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
   ),
   resumeLink:
     "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
